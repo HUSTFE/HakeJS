5720137d265ee4c4db2d84996a00598b1e5948f5
xuzhi.ming <xuzhi.ming@qunar.com>
2017-08-14 19:41:21 +0800

    opti: change hash function.

--- src/data/index.js
+++ src/data/index.js
@@ -1,14 +1,15 @@
 let dataReg = /{{[^(?!.*{{)]*}}/g;
 
-function dataHash(str, num) {
-  let hash = 1315423911, i, ch;
+function dataHash(str) {
+  let hash = 1713302033171, i, ch;
 
   for (i = str.length - 1; i >= 0; i--) {
     ch = str.charCodeAt(i);
     hash ^= ((hash << 5) + ch + (hash >> 2));
+    hash ^= ((hash >> 2) + ch + (hash << 5));
   }
 
-  return str + '-' + (hash & num).toString(16);
+  return (hash).toString(16);
 }
 
 function hasData(str) {
